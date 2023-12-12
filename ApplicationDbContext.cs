0fabc5f4c94b2d641eaf41eace9ee40959be4947
MursleenButt <150161430+MursleenButt@users.noreply.github.com>
2023-12-13 00:00:25 +0500

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Assignment_2.Model;
+using Assignment_2.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+        {
+   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+            }
+
+            public DbSet<Student> Students { get; set; }
+   public DbSet<Class> Classs { get; set; }
+    public DbSet<Faculty> Facultys { get; set; }
+    public DbSet<Enrolled> Enrolleds { get; set; }
+}
+    
+
+
